8e8a384dc55f3279cbb421827e3b6cd2fa37d722
VasyaSmetankin <meemkook@icloud.com>
2025-01-14 21:53:33 +0300

    Configure Spring beans for Koschei's death chain with setter, constructor, and @Bean methods

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -5,12 +5,13 @@ import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
-
     @Bean
+    @Primary
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
